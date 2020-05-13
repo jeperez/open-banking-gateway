9c5aaabdcbd043341315ceccb2d474f525d99d6b
iiuex <iiu@adorsys.com.ua>
2020-05-13 10:52:08 +0300

    OBG-493: init commit, required fields are signed by fintech

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/config/FeignConfig.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/config/FeignConfig.java
@@ -12,6 +12,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.COMPUTE_PSU_IP_ADDRESS;
@@ -20,6 +22,16 @@ import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.X_REQUEST_ID;
 import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.X_OPERATION_TYPE;
 import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.X_REQUEST_SIGNATURE;
 import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.X_TIMESTAMP_UTC;
+import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.BANK_ID;
+import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.FINTECH_USER_ID;
+import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.FINTECH_REDIRECT_URL_OK;
+import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.FINTECH_REDIRECT_URL_NOK;
+import static de.adorsys.opba.fintech.impl.tppclients.QueryFields.DATE_FROM;
+import static de.adorsys.opba.fintech.impl.tppclients.QueryFields.DATE_TO;
+import static de.adorsys.opba.fintech.impl.tppclients.QueryFields.ENTRY_REFERENCE_FROM;
+import static de.adorsys.opba.fintech.impl.tppclients.QueryFields.BOOKING_STATUS;
+import static de.adorsys.opba.fintech.impl.tppclients.QueryFields.DELTA_LIST;
+import static de.adorsys.opba.fintech.impl.tppclients.QueryFields.KEYWORD;
 
 /**
  * This class enhances requests, so that the include PSU IP address headers. This header should either become
@@ -30,6 +42,7 @@ import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.X_TIMESTAMP_U
 @Configuration
 @RequiredArgsConstructor
 public class FeignConfig {
+    private static final String MISSING_HEADER_ERROR_MESSAGE = " header is missing";
     private final RequestSigningService requestSigningService;
     private final TppProperties tppProperties;
 
@@ -44,15 +57,95 @@ public class FeignConfig {
 
     private void fillSecurityHeaders(RequestTemplate requestTemplate) {
         Instant instant = Instant.now();
-        requestTemplate.header(X_REQUEST_SIGNATURE, calculateSignature(requestTemplate.request(), instant));
+        requestTemplate.header(X_REQUEST_SIGNATURE, calculateSignature(requestTemplate, instant));
         requestTemplate.header(FINTECH_ID, tppProperties.getFintechID());
         requestTemplate.header(X_TIMESTAMP_UTC, instant.toString());
     }
 
-    private String calculateSignature(Request request, Instant instant) {
-        String xRequestId = request.headers().get(X_REQUEST_ID).stream().findFirst().orElse(null);
-        String operationType = request.headers().get(X_OPERATION_TYPE).stream().findFirst().orElse(null);
-        DataToSign dataToSign = new DataToSign(UUID.fromString(xRequestId), instant, OperationType.valueOf(operationType));
+    private String calculateSignature(RequestTemplate requestTemplate, Instant instant) {
+        String operationType = requestTemplate.headers().get(X_OPERATION_TYPE).stream().findFirst().orElseThrow(() -> new IllegalStateException(X_OPERATION_TYPE + MISSING_HEADER_ERROR_MESSAGE));
+        String xRequestId = requestTemplate.headers().get(X_REQUEST_ID).stream().findFirst().orElseThrow(() -> new IllegalStateException(X_REQUEST_ID + MISSING_HEADER_ERROR_MESSAGE));
+        DataToSign dataToSign;
+
+        switch (operationType) {
+            case "AIS": dataToSign = buildAisDataToSign(requestTemplate, instant, operationType, xRequestId);
+            break;
+            case "BANK_SEARCH": dataToSign = buildBankSearchDataToSign(requestTemplate, instant, operationType, xRequestId);
+            break;
+            case "CONFIRM_CONSENT": dataToSign = buildConfirmConsentDataToSign(
+                    instant, operationType, xRequestId);
+            break;
+            default: throw new IllegalArgumentException("Unsupported operation type " + operationType);
+        }
+
         return requestSigningService.signature(dataToSign);
+    }
+
+    private DataToSign buildAisDataToSign(RequestTemplate requestTemplate, Instant instant, String operationType, String xRequestId) {
+        String bankId = requestTemplate.headers().get(BANK_ID).stream()
+                                .findFirst()
+                                .orElse("");
+        String fintechUserId = requestTemplate.headers().get(FINTECH_USER_ID).stream()
+                                       .findFirst()
+                                       .orElseThrow(() -> new IllegalStateException(FINTECH_USER_ID + MISSING_HEADER_ERROR_MESSAGE));
+        String redirectOkUrl = requestTemplate.headers().get(FINTECH_REDIRECT_URL_OK).stream()
+                                       .findFirst()
+                                       .orElseThrow(() -> new IllegalStateException(FINTECH_REDIRECT_URL_OK + MISSING_HEADER_ERROR_MESSAGE));
+        String redirectNokUrl = requestTemplate.headers().get(FINTECH_REDIRECT_URL_NOK).stream()
+                                        .findFirst()
+                                        .orElseThrow(() -> new IllegalStateException(FINTECH_REDIRECT_URL_NOK + MISSING_HEADER_ERROR_MESSAGE));
+
+        Map<String, String> extraValues = new HashMap<>();
+        extraValues.put(BANK_ID, bankId);
+        extraValues.put(FINTECH_USER_ID, fintechUserId);
+        extraValues.put(FINTECH_REDIRECT_URL_OK, redirectOkUrl);
+        extraValues.put(FINTECH_REDIRECT_URL_NOK, redirectNokUrl);
+
+        if (requestTemplate.path().contains("/transactions")) {
+            addTransactionsQueries(requestTemplate, extraValues);
+        }
+
+        return new DataToSign(UUID.fromString(xRequestId), instant, OperationType.valueOf(operationType), extraValues);
+    }
+
+    private DataToSign buildBankSearchDataToSign(RequestTemplate requestTemplate, Instant instant, String operationType, String xRequestId) {
+        if (!requestTemplate.path().contains("/bank-search")) {
+            return new DataToSign(UUID.fromString(xRequestId), instant, OperationType.valueOf(operationType));
+        }
+
+        String keyword = requestTemplate.queries().get(KEYWORD).stream()
+                                  .findFirst()
+                                 .orElseThrow(() -> new IllegalStateException(KEYWORD + MISSING_HEADER_ERROR_MESSAGE));
+        Map<String, String> extraValues = new HashMap<>();
+        extraValues.put(KEYWORD, keyword);
+        return new DataToSign(UUID.fromString(xRequestId), instant, OperationType.valueOf(operationType), extraValues);
+    }
+
+    private DataToSign buildConfirmConsentDataToSign(Instant instant, String operationType, String xRequestId) {
+        return new DataToSign(UUID.fromString(xRequestId), instant, OperationType.valueOf(operationType));
+    }
+
+    private void addTransactionsQueries(RequestTemplate requestTemplate, Map<String, String> extraValues) {
+        String dateFrom = requestTemplate.queries().get(DATE_FROM).stream()
+                                .findFirst()
+                                .orElse("");
+        String dateTo = requestTemplate.queries().get(DATE_TO).stream()
+                                .findFirst()
+                                .orElse("");
+        String entryReferenceFrom = requestTemplate.queries().get(ENTRY_REFERENCE_FROM).stream()
+                                .findFirst()
+                                .orElse("");
+        String bookingStatus = requestTemplate.queries().get(BOOKING_STATUS).stream()
+                                .findFirst()
+                                .orElse("");
+        String deltaList = requestTemplate.queries().get(DELTA_LIST).stream()
+                                .findFirst()
+                                .orElse("");
+
+        extraValues.put(DATE_FROM, dateFrom);
+        extraValues.put(DATE_TO, dateTo);
+        extraValues.put(ENTRY_REFERENCE_FROM, entryReferenceFrom);
+        extraValues.put(BOOKING_STATUS, bookingStatus);
+        extraValues.put(DELTA_LIST, deltaList);
     }
 }
